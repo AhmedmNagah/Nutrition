f8c8d8e0326e477a860cf2ef7c5dfbf7532b1b7d
ahmedMNagah <anagah@unitedofoq.com>
2021-12-23 15:39:44 +0200

    first commit in nutrition app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ResponceComponent } from './pages/responce/responce.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ResponceComponent,
+   
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+function routes(routes: any): any[] | import("@angular/core").Type<any> {
+  throw new Error('Function not implemented.');
+}
+
